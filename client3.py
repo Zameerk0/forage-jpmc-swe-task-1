f44caeea88256ad629f901d063018c20a7674ec5
Zameerk0 <sk6761987@gmail.com>
2024-10-06 15:25:51 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -40,7 +40,7 @@ def getDataPoint(quote):
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
+    """ Get ratio of prices_a and price_b """
     """ ------------- Update this function ------------- """
     return 1
 
