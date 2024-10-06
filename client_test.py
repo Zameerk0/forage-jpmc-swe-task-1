60752ce5b205e39d2e1e3bf5da06bedd89199d98
Zameerk0 <sk6761987@gmail.com>
2024-10-06 15:26:51 +0530

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -17,7 +17,7 @@ class ClientTest(unittest.TestCase):
     """ ------------ Add the assertion below ------------ """
 
 
-  """ ------------ Add more unit tests ------------ """
+  """ ------------ Add more unit test ------------ """
 
 
 
